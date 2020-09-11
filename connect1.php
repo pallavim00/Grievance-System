acdfaac58ff772cf93b4de760bc99c7a98c935fd
devanshshah59 <devanshshah248@gmail.com>
2020-09-11 20:27:53 +0530

    abcdefghijkl

--- connect1.php
+++ connect1.php
@@ -14,11 +14,21 @@
              die(' Connection Failed! : ' .$conn->connect_error);
      }
      else  {
+
+       $q = "select * from student where username = '$username' ";
+       $result = mysqli_query($conn , $q);
+       $num = mysqli_num_rows($result);
+       if($num == 1){
+              echo " pls try other username";
+       }
+       else  {
             $stmt = $conn->prepare("insert into student(username, studentname, institutename, departmentname, emailid, contactno, password)values(?,?,?,?,?,?,?)");
             $stmt->bind_param("sssssis",$username, $studentname, $institutename, $departmentname, $emailid, $contactno, $password) ;
+
             $stmt->execute();
             echo "Registration done Successfully...";
             $stmt->close();
             $conn->close();
+       }
      }
 ?>
