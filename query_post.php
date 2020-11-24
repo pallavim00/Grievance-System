6b81a7dbe7edb9dbd0a1c34a2d2e4e906fada1db
devanshshah59 <devanshshah248@gmail.com>
2020-11-24 17:04:52 +0530

    abc

--- query_post.php
+++ query_post.php
@@ -8,9 +8,9 @@
         $description = $_POST['description'];
     
         
-        $sql = $con->prepare("INSERT INTO query_post(level, query_title, description) values(?,?,?)");
+        $sql = $con->prepare("insert into query_post(level, query_title, description)values(?,?,?)");
         $sql->bind_param("sss",$level, $query_title, $description);
-        $sql->execute();
+        $sql->execute();        
         if(!mysqli_query($con,$sql))
         {
             echo 'not inserted';
@@ -19,6 +19,6 @@
         {
             echo "<script type='text/javascript'>alert('Query has been Posted Successfully.')</script>";
         }
-        header("location:query.html");
+        header("location:post.html");
     }   
 ?>
